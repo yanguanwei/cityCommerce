9ce6c81b7f6eda2eca1987e0671e4a88a13fb473
yanguanwei <young.alway@gmail.com>
2017-05-14 20:07:15 +0800

    init

---
+++ bin/restart.sh
@@ -0,0 +1,9 @@
+project_home=/home/ubuntu
+project_name=cityCommerce
+cd $project_home/$project_name
+pid=`ps -ef |grep $project_name |grep -v "grep" |awk '{print $2}' `
+if [ $pid ]; then
+    echo "${project_name}  is  running  and pid=$pid"
+    kill -9 $pid
+fi
+nohup java -jar $project_home/$project_name/target/$project_name-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
